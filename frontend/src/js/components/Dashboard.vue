8d7bff1d7907f43876856308969ddbc13f214661
peddn <public@peddn.net>
2023-02-22 13:42:50 +0100

    implemented restricted view

--- frontend/src/js/components/Dashboard.vue
+++ frontend/src/js/components/Dashboard.vue
@@ -1,12 +1,16 @@
 <script setup>
-  import { onBeforeRouteLeave } from 'vue-router'
-  import { useAppStore } from '../stores/app.js'
-  const appStore = useAppStore()
-  onBeforeRouteLeave((to, from) => {
-    appStore.setMenuItemActive(to.name)
-  })
+  import { useUserStore } from '../stores/user.js'
+
+  import AccessDenied from './user/AccessDenied.vue'
+
+  const userStore = useUserStore()
 </script>
 
 <template>
-  <h1>Dashboard</h1>
+  <div v-if="userStore.authenticated">
+    <h1>{{ userStore.user.username }}'s dashboard</h1>
+  </div>
+  <div v-else>
+    <AccessDenied />
+  </div>
 </template>
