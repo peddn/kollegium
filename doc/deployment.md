4ee1e00c48b8d2a4bf5b1bcc62e751143992d6a5
peddn <public@peddn.net>
2023-05-30 16:10:18 +0200

    fixed issues with .env and populating user with extra data

--- doc/deployment.md
+++ doc/deployment.md
@@ -3,3 +3,5 @@
 command to generate secrets
 
 python -c "import base64, secrets; print(base64.b64encode(secrets.token_bytes(16)).decode('utf-8'))"
+
+node crypto.randomBytes(16).toString('base64')
--- frontend/src/js/api/user.js
+++ frontend/src/js/api/user.js
@@ -5,6 +5,8 @@ const API_URL = process.env.API_URL
 export async function userLogin(identifier, password) {
   let response
 
+  // get the jwt und user data form the auth process
+  let data
   try {
     response = await fetch(`${API_URL}/api/auth/local`, {
       method: 'POST',
@@ -19,22 +21,20 @@ export async function userLogin(identifier, password) {
   } catch (error) {
     throw new NetworkError(error.message)
   }
-
   if (!response.ok) {
     const errorMessage = response.status + ': ' + response.statusText
     throw new HTTPError(errorMessage)
   }
-
-  let data
-
   try {
     data = await response.json()
   } catch (error) {
     throw new ParsingError(error.message)
   }
 
+  // fetch the user and populate with extra data
+  let user
   try {
-    response = await fetch(`${API_URL}/api/users/me?populate=role`, {
+    response = await fetch(`${API_URL}/api/users/me?populate[0]=role,avatar`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -44,22 +44,18 @@ export async function userLogin(identifier, password) {
   } catch (error) {
     throw new NetworkError(error.message)
   }
-
   if (!response.ok) {
     const errorMessage = response.status + ': ' + response.statusText
     throw new HTTPError(errorMessage)
   }
-
-  let user
-
   try {
     user = await response.json()
   } catch (error) {
     throw new ParsingError(error.message)
   }
 
-  console.log('user', user)
+  // overwrite the old user with the updated user
   data.user = user
-  console.log('data', data)
+
   return data
 }
